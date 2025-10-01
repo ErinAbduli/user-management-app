5fb0e3d2cb05065a1196dded87ffbeaa07191519
ErinAbduli <erinabduli7@gmail.com>
2025-10-01 19:38:53 +0200

    setup initial pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Button } from "@chakra-ui/react";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import UserDetails from "./pages/UserDetails";
 
 function App() {
 	return (
-		<>
-			<Button>LCICK</Button>
-		</>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/user/:id" element={<UserDetails />} />
+		</Routes>
 	);
 }
 
